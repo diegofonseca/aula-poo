9f7fefb63a8073fc7d9152b8d85e3d7d924a5600
fonseca <dfonseca404@gmail.com>
2018-10-22 10:53:14 -0400

    Initial commit

---
+++ Poo/src/br/ulbra/poo/App.java
@@ -0,0 +1,11 @@
+package br.ulbra.poo;
+
+public class App {
+    /**
+     * Método principal da aplicação, irá executar ao iniciar aplicação
+     * @param args  Parâmetros enviados via linha de comandos irão para esta variável
+     */
+    public static void main(String[] args) {
+        
+    }
+}
