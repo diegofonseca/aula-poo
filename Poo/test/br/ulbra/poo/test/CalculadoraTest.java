c453a436556cac5120fa5d4d4896ceaba5cf681b
Labin 05 <Labin 05@JPR-L166147>
2018-10-22 20:11:54 -0400

    Media ponderada

--- Poo/test/br/ulbra/poo/test/CalculadoraTest.java
+++ Poo/test/br/ulbra/poo/test/CalculadoraTest.java
@@ -6,6 +6,16 @@ import static org.junit.Assert.*;
 
 public class CalculadoraTest {
     @Test
+    public void mediaPonderada() {
+        Calculadora c = new Calculadora();
+        int resultado1 = c.mediaPonderada(80,50);
+        int resultado2 = c.mediaPonderada(80,65);
+                    
+        assertEquals(60, resultado1);
+        assertEquals(70, resultado2);
+    }
+    
+    @Test
     public void somaTest() {
         Calculadora c = new Calculadora();
         int resultado = c.soma(2,2);
